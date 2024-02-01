5cddb741da9cbc91c746788c1f98a69b98b1d41a
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-01 16:04:41 +0530

    Update docker_scout_step2.sh
    Docker Login is must

--- Docker Scout/docker_scout_step2.sh
+++ Docker Scout/docker_scout_step2.sh
@@ -1,5 +1,6 @@
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-docker login
+docker login #docker login is must because without logging in to the dockerhub account we can't do further scout installation process, it will give error
+
 #Given commands below is for docker scout installtion
 curl -fsSL https://raw.githubusercontent.com/docker/scout-cli/main/install.sh -o install-scout.sh
 sh install-scout.sh
