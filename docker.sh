1bb18edee15d8ba223a104f4aa7222aaab19cd80
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-02 15:37:41 +0530

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install docker.io -y
+newgrp docker
+sudo usermod -aG docker $USER
+sudo chmod 777 /var/run/docker.sock
+#sudo usermod -aG docker jenkins
+#sudo usermod -aG docker $USER
+#sudo rebooot
