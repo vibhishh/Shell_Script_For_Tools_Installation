11bb62c4a9fa0e9e1de748762900f550b7e18827
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-02 15:57:36 +0530

    Update nodeJS.sh

--- nodeJS.sh
+++ nodeJS.sh
@@ -1,3 +1,4 @@
+#!bin/bash
 # Install Node.js 16 and npm
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_16.x focal main" | sudo tee /etc/apt/sources.list.d/nodesource.list
