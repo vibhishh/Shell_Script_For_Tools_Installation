0fb9c6b1c48c1c48b1d78d639b8e21e2e735b538
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-02 16:32:16 +0530

    Create  kubeadm_worker.sh

---
+++ kubernetes/kubeadm_worker.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+sudo su
+apt update -y
+apt install docker.io -y
+
+systemctl start docker
+systemctl enable docker
+
+curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
+echo 'deb https://packages.cloud.google.com/apt kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list
+
+apt update -y
+apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+
+kubeadm reset pre-flight checks
+
+#Now Paste the token at worker node which we copied from the master node with append at the end of the join command [--v=5]
