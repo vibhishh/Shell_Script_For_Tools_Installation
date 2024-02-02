5f8f190f6e60fd24196dd44e189afb4dba566465
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-02 16:11:28 +0530

    Create minikube.sh

---
+++ kubernetes/minikube.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+sudo apt update
+sudo apt install -y curl wget apt-transport-https
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
+#kubectl 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+sudo usermod -aG docker $USER && newgrp docker
+
+minikube start --driver=docker
+minikube status
