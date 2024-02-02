09bd5c2ed46212cc299423587738ec24d977424a
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-02 16:59:16 +0530

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -28,8 +28,12 @@ sudo systemctl enable prometheus
 sudo systemctl start prometheus
 sudo systemctl status prometheus
 
-#Suppose you encounter any issues with Prometheus or are unable to start it. The easiest way to find the problem is to use the journalctl command and search for errors.
+# Systemd unit configuration file.
+#sudo vim /etc/systemd/system/prometheus.service
 
+#Suppose you encounter any issues with Prometheus or are unable to start it. The easiest way to find the problem is to use the journalctl command and search for errors.
 journalctl -u prometheus -f --no-pager
 
 #More at https://mrcloudbook.com/netflix-clone-ci-cd-with-monitoring-email-devsecops/
+
+#copy public ip with port no 9090 -- access to the chrome  DONE
