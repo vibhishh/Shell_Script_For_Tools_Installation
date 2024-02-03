0e1890ec2700b660d48f64b2add70a291eaf2eec
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-03 07:59:02 +0530

    Create grafana.sh

---
+++ grafana.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+sudo apt-get install -y apt-transport-https software-properties-common
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get -y install grafana
+sudo systemctl enable grafana-server
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+
+#copy the public ip of the instance paste to chrome along with port no 3000 (Grafana Port)
