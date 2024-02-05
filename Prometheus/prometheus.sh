a5c463c01afc8453c6f6e96cf9937fc5819f91f7
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-05 09:26:23 +0530

    Update prometheus.sh

--- Prometheus/prometheus.sh
+++ Prometheus/prometheus.sh
@@ -26,13 +26,38 @@ sudo chown -R prometheus:prometheus /etc/prometheus/ /data/
 #rm -rf prometheus-2.47.1.linux-amd64.tar.gz
 
 prometheus --version
+
+#sudo vim /etc/systemd/system/prometheus.service [copy all upto multi-user.target and paste to prometheus.service file]
+
+#[Unit]
+#Description=Prometheus
+#Wants=network-online.target
+#After=network-online.target
+#StartLimitIntervalSec=500
+#StartLimitBurst=5
+#[Service]
+#User=prometheus
+#Group=prometheus
+#Type=simple
+#Restart=on-failure
+#RestartSec=5s
+#ExecStart=/usr/local/bin/prometheus \
+# --config.file=/etc/prometheus/prometheus.yml \
+#  --storage.tsdb.path=/data \
+#  --web.console.templates=/etc/prometheus/consoles \
+#  --web.console.libraries=/etc/prometheus/console_libraries \
+#  --web.listen-address=0.0.0.0:9090 \
+#  --web.enable-lifecycle
+#[Install]
+#WantedBy=multi-user.target
+
 sudo systemctl enable prometheus
+sudo systemctl start prometheus
+sudo systemctl status prometheus
+
 
 #Suppose you encounter any issues with Prometheus or are unable to start it. The easiest way to find the problem is to use the journalctl command and search for errors.
 journalctl -u prometheus -f --no-pager
-
-sudo systemctl start prometheus
-sudo systemctl status prometheus
 
 # Systemd unit configuration file.
 #sudo vim /etc/systemd/system/prometheus.service
