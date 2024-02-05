d5c5ca26d874f9ca9e46aa3526497bb1de124fb3
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-05 09:13:47 +0530

    Update prometheus.sh

--- Prometheus/prometheus.sh
+++ Prometheus/prometheus.sh
@@ -27,14 +27,15 @@ sudo chown -R prometheus:prometheus /etc/prometheus/ /data/
 
 prometheus --version
 sudo systemctl enable prometheus
+
+#Suppose you encounter any issues with Prometheus or are unable to start it. The easiest way to find the problem is to use the journalctl command and search for errors.
+journalctl -u prometheus -f --no-pager
+
 sudo systemctl start prometheus
 sudo systemctl status prometheus
 
 # Systemd unit configuration file.
 #sudo vim /etc/systemd/system/prometheus.service
-
-#Suppose you encounter any issues with Prometheus or are unable to start it. The easiest way to find the problem is to use the journalctl command and search for errors.
-journalctl -u prometheus -f --no-pager
 
 #More at https://mrcloudbook.com/netflix-clone-ci-cd-with-monitoring-email-devsecops/
 
