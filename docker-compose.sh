a6a5eedcbb8a6fa127ad9c032390c563bbc2428e
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-05 08:55:04 +0530

    Create docker-compose.sh

---
+++ docker-compose.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose --version
