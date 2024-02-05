301877e3ff4bffdcaabe6b05a0ba34d26e21fcd0
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-05 09:31:13 +0530

    Update node_exporter.sh

--- Prometheus/node_exporter.sh
+++ Prometheus/node_exporter.sh
@@ -18,6 +18,23 @@ node_exporter --version
 #node_exporter.service
 #sudo vim /etc/systemd/system/node_exporter.service
 
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+StartLimitIntervalSec=500
+StartLimitBurst=5
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/node_exporter \
+    --collector.logind
+[Install]
+WantedBy=multi-user.target
+
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
 sudo systemctl status node_exporter
