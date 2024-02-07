ffe0a4bb38bdbf648483eb65e50bbefa156ebfe9
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-07 14:53:49 +0530

    Update kubeadm_master.sh

--- kubernetes/kubeadm_master.sh
+++ kubernetes/kubeadm_master.sh
@@ -7,11 +7,12 @@ apt install docker.io -y
 systemctl start docker
 systemctl enable docker
 
-curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
-echo 'deb https://packages.cloud.google.com/apt kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-apt update -y
-apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
 kubeadm init
 mkdir -p $HOME/.kube
