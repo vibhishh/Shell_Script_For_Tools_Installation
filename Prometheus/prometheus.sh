0112bd617bfb72ad5a51b20d213e61913fc2b5cf
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2024-02-08 12:15:14 +0530

    Update prometheus.sh

--- Prometheus/prometheus.sh
+++ Prometheus/prometheus.sh
@@ -29,27 +29,27 @@ prometheus --version
 
 #sudo vim /etc/systemd/system/prometheus.service [copy all upto multi-user.target and paste to prometheus.service file]
 
-#[Unit]
-#Description=Prometheus
-#Wants=network-online.target
-#After=network-online.target
-#StartLimitIntervalSec=500
-#StartLimitBurst=5
-#[Service]
-#User=prometheus
-#Group=prometheus
-#Type=simple
-#Restart=on-failure
-#RestartSec=5s
-#ExecStart=/usr/local/bin/prometheus \
-# --config.file=/etc/prometheus/prometheus.yml \
-#  --storage.tsdb.path=/data \
-#  --web.console.templates=/etc/prometheus/consoles \
-#  --web.console.libraries=/etc/prometheus/console_libraries \
-#  --web.listen-address=0.0.0.0:9090 \
-#  --web.enable-lifecycle
-#[Install]
-#WantedBy=multi-user.target
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+StartLimitIntervalSec=500
+StartLimitBurst=5
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/bin/prometheus \
+ --config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/data \
+  --web.console.templates=/etc/prometheus/consoles \
+  --web.console.libraries=/etc/prometheus/console_libraries \
+  --web.listen-address=0.0.0.0:9090 \
+  --web.enable-lifecycle
+[Install]
+WantedBy=multi-user.target
 
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
