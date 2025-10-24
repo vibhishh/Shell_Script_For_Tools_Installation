361a82e0c5882b2cfc907fb0307657072cd2e050
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2025-10-24 18:39:02 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,9 +2,6 @@
 sudo apt update
 sudo apt install fontconfig openjdk-21-jre
 java -version
-openjdk version "21.0.3" 2024-04-16
-OpenJDK Runtime Environment (build 21.0.3+11-Debian-2)
-OpenJDK 64-Bit Server VM (build 21.0.3+11-Debian-2, mixed mode, sharing)
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
                   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
