c87484be63718e0a333f0eedb90c6f8aefea1714
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2025-12-01 03:07:29 +0530

    Create K8s_Master.sh

---
+++ kubernetes/New_K8s/K8s_Master.sh
@@ -0,0 +1,74 @@
+#!bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
+
+#Install Required Packages
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
+
+#Disable Swap
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+#Install and Configure containerd
+sudo apt-get install -y containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
+
+#Enable SystemdCgroup
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+
+#Restart and enable service:
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+
+#Add Kubernetes v1.31 APT Repository
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key |
+sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+#Install Kubernetes Components
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+#Load Required Kernel Modules
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+#Configure Network Settings for Kubernetes
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+#Install conntrack
+sudo apt-get update
+sudo apt-get install -y conntrack
+sudo apt-get install -y socat ebtables ethtool
+
+#Initialize Kubernetes Control Plane
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=$PRIVATE_IP
+
+#Set up kubectl
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+ #Install Calico CNI (For v1.31 Compatibility)
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/custom-resources.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.1/manifests/calico.yaml
+
+#Check pods Status
+kubectl get pods -A
+
+#Verify Kubernetes Cluster Status
+kubectl get nodes
+
