959274c54ea93722a1c774f6fac9d50a2c776923
Mradul Singh <78133213+vibhishh@users.noreply.github.com>
2026-08-15 02:52:25 +0530

    Update jenkins.sh
    Add new installation commands for Jenkins.

--- jenkins.sh
+++ jenkins.sh
@@ -11,3 +11,21 @@ sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl status jenkins
+
+#If Above not Work
+sudo apt update -y
+sudo apt install fontconfig openjdk-21-jre -y
+java -version
+
+sudo mkdir -p /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+  
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | \
+  sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
